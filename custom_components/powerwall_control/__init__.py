7588496595cb5720dd523e5f5ecd9a2c30466ae5
David Kilroy <kilroyd@googlemail.com>
2025-10-16 19:43:56 +0100

    Initial definition of PwCtrlConfigEntry

--- custom_components/powerwall_control/__init__.py
+++ custom_components/powerwall_control/__init__.py
@@ -1,13 +1,18 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
+# Our ConfigEntry.runtime_data will hold PwCtrlData.
+# Otherwise access the config entries the the .data[] dictionary.
+type PwCtrlConfigEntry = ConfigEntry[PwCtrlData]
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+
+async def async_setup_entry(hass: HomeAssistant, entry: PwCtrlConfigEntry) -> bool:
     """
     Set up Powerwall Control from a config entry .
     """
     # TODO: initialize class that will do the talking to Netzero?
-    entry.runtime_data = None
+    entry.runtime_data = PwCtrlData(hass, entry.data["api_token"], entry.data["system_id"])
+
     return True
 
 
@@ -16,3 +21,13 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     Unload a config entry.
     """
     return True
+
+
+# Temp location. Move to appropriate file when we figure out what we
+# want PwCtrlData to do. For now store the token and system id.
+class PwCtrlData:
+    def __init__(self, hass: HomeAssistant, api_token:str, system_id: str) -> None:
+        """Init dummy hub."""
+        self._hass = hass
+        self._api_token = api_token
+        self._system_id = system_id
