864ef9a1a155decbe0e20aad1dcfd9897b4d3aa6
David Kilroy <kilroyd@googlemail.com>
2025-10-18 11:57:20 +0100

    netzero: create a python module

---
+++ netzero/__init__.py
@@ -0,0 +1,12 @@
+"""Netzero Developer API package."""
+
+from .netzero import (
+    Auth as Auth,
+    EnergyExportMode as EnergyExportMode,
+    EnergySite as EnergySite,
+    EnergySiteConfig as EnergySiteConfig,
+    EnergySiteStatus as EnergySiteStatus,
+    GridStatus as GridStatus,
+    IslandStatus as IslandStatus,
+    OperationalMode as OperationalMode,
+)
