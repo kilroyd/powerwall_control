f680abfc39299753b03b31974d4f7a24c3f5b780
David Kilroy <kilroyd@googlemail.com>
2025-10-18 20:06:26 +0100

    Make switch async

--- custom_components/powerwall_control/switch.py
+++ custom_components/powerwall_control/switch.py
@@ -32,11 +32,11 @@ class PwCtrlGridChargingSwitch(SwitchEntity):
         """If the switch is currently on or off."""
         return self._is_on
 
-    def turn_on(self, **kwargs):
+    async def async_turn_on(self, **kwargs):
         """Turn the switch on."""
         self._is_on = True
 
-    def turn_off(self, **kwargs):
+    async def async_turn_off(self, **kwargs):
         """Turn the switch off."""
         self._is_on = False
 
