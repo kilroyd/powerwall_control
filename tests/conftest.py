85959329c31d4a9ffe1bcab5a0d91a6bb3fb1b5c
David Kilroy <kilroyd@googlemail.com>
2025-11-15 20:30:51 +0000

    testing: declare a common fixture for mock EnergySite
    Change the main code to import netzero in such a way that we
    can mock it at a single location.
    
    Now simplify the tests to use mock_energysite.
    
    Remove banned import for `tests`, as we want to import mocks
    from confdata.py. We'll figure out how if we need to reorganise
    later. For now, it's nice to have the netzero mock setup in one
    place.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,15 @@
 """Fixtures for testing."""
 
+from unittest.mock import AsyncMock, patch
+
 import pytest
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+
+from custom_components.powerwall_control.const import DOMAIN
+from custom_components.powerwall_control.netzero import EnergySiteConfig
+from homeassistant.core import HomeAssistant
+
+from .mocks import _mock_energysite_get_config
 
 
 @pytest.fixture(autouse=True)
@@ -11,3 +20,36 @@ def auto_enable_custom_integrations(enable_custom_integrations):
     loads the custom components in independent repositories.
     """
     return
+
+
+DEFAULT_JSON = {
+    "backup_reserve_percent": 80,
+    "operational_mode": "autonomous",
+    "energy_exports": "pv_only",
+    "grid_charging": False,
+}
+
+
+@pytest.fixture(name="mock_energysite")
+async def mock_energysite_fixture(hass: HomeAssistant) -> AsyncMock:
+    """This fixture provides a mock EnergySite, returning constant EnergySiteConfig."""
+    mock_energysite = await _mock_energysite_get_config(
+        EnergySiteConfig(123456, DEFAULT_JSON)
+    )
+
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "api_token": "ABCDEFG",
+            "system_id": "123456",
+        },
+    )
+    entry.add_to_hass(hass)
+
+    with patch(
+        "custom_components.powerwall_control.netzero.EnergySite",
+        return_value=mock_energysite,
+    ):
+        assert await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+        yield mock_energysite
