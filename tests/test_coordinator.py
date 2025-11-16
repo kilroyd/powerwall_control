kilroyd/powerwall_control

commit d5a66cca808e3dc2356faf0c8e9feffe61282918

Refactor to simplify netzero objects
Avoid allowing EnergySiteConfig and EnergySiteStatus to update
themselves. Always do this through EnergySite. Move modification
of the configuration to EnergySite.async_set_config().

Add __eq__ functions to compare each data object. I've made these
only compare the current object, and not compare recursively. This
allows us to avoid updating config properties when the wall
connector data changes.

This allows us to fix the coordinator to only update on change.
Set the DataUpdateCoordinator generic type to EnergySiteConfig.
Update the entities to reference their data from coordinator.data.
Return the EnergySiteConfig from _async_update_data.

custom_components/powerwall_control/coordinator.py
@@ -1,14 +1,13 @@
 """Data update coordinator."""
 
 from datetime import timedelta
-from typing import Any
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.debounce import Debouncer
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 from .const import DOMAIN, LOGGER
-from .netzero import EnergySiteConfig
+from .netzero import EnergySite, EnergySiteConfig
 
 # This integration is making configuration data available, which
 # generally shouldn't be changing, except where an automation is
@@ -19,7 +18,7 @@ REQUEST_CONTROL_DEFAULT_COOLDOWN = 15
 REQUEST_CONTROL_DEFAULT_IMMEDIATE = False
 
 
-class PwCtrlCoordinator(DataUpdateCoordinator[dict[str, Any]]):
+class PwCtrlCoordinator(DataUpdateCoordinator[EnergySiteConfig]):
     """Class used to manage data collection.
 
     The Netzero API returns the status of multiple entities in a
@@ -32,20 +31,19 @@ class PwCtrlCoordinator(DataUpdateCoordinator[dict[str, Any]]):
     single call.
     """
 
-    def __init__(self, hass: HomeAssistant, config: EnergySiteConfig) -> None:
+    def __init__(self, hass: HomeAssistant, site: EnergySite) -> None:
         """Initialize coordinator."""
         super().__init__(
             hass,
             logger=LOGGER,
             name=DOMAIN,
             update_interval=UPDATE_INTERVAL,
-            # The API updates the data in config, and we don't return json in _async_update_data.
-            # So tell the DataUpdateCoordinator to always update all entities.
-            always_update=True,
+            # Filter out no-op updates
+            always_update=False,
         )
-        # This object contains acceesors for all the data retreived,
-        # and has an async_update call to refresh the data.
-        self.config = config
+        # This object contains async functions to get updated data,
+        # and set desired state.
+        self.site = site
 
         # List of things we want to set on the next config call
         self._reconfig_dict = {}
@@ -58,13 +56,12 @@ class PwCtrlCoordinator(DataUpdateCoordinator[dict[str, Any]]):
             function=self._async_control,
         )
 
-    async def _async_update_data(self) -> dict[str, Any]:
+    async def _async_update_data(self) -> EnergySiteConfig:
         """Refresh data."""
         # ClientErrors are caught by DataUpdateCoordinator.  Netzero
         # isn't raising any more specific errors, so just allow the
         # default handling to take care of it.
-        await self.config.async_update()
-        return {}
+        return await self.site.async_get_config()
 
     async def async_request_control(self, **kwargs) -> None:
         """Pass requests for control to netzero.
@@ -79,8 +76,8 @@ class PwCtrlCoordinator(DataUpdateCoordinator[dict[str, Any]]):
     async def _async_control(self) -> None:
         """Invoke the control call, and update listeners with result."""
         # Pass the accumulated configuration changes to netzero
-        await self.config.async_control(**self._reconfig_dict)
+        updated_config = await self.config.async_set_config(**self._reconfig_dict)
         self._reconfig_dict.clear()
 
         # Update listeners with any new values
-        self.async_set_updated_data({})
+        self.async_set_updated_data(updated_config)

custom_components/powerwall_control/__init__.py
@@ -76,7 +76,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: PwCtrlConfigEntry) -> bo
         hass, entry.data["api_token"], entry.data["system_id"]
     )
 
-    coordinator = PwCtrlCoordinator(hass, config)
+    coordinator = PwCtrlCoordinator(hass, site)
 
     entry.runtime_data = PwCtrlData(hass, site, coordinator, device_info)
 
@@ -86,7 +86,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: PwCtrlConfigEntry) -> bo
 
     # Update all entities with initial values populated during
     # async_get_config() call
-    coordinator.async_set_updated_data({})
+    coordinator.async_set_updated_data(config)
 
     return True
 

custom_components/powerwall_control/netzero.py
@@ -67,6 +67,15 @@ class WallConnector:
         """Initialize a Wall Connector object."""
         self.raw_data = raw_data
 
+    def __eq__(self, other: "WallConnector") -> bool:
+        """Compare WallConnector objects."""
+        return (
+            self.din == other.din
+            and self.state == other.state
+            and self.fault_state == other.fault_state
+            and self.power == other.power
+        )
+
     @property
     def din(self) -> str:
         """Identifying number."""
@@ -93,13 +102,27 @@ class WallConnector:
 class EnergySiteStatus:
     """Class that represents an energy site's current status."""
 
-    def __init__(
-        self, auth: Auth, site_id: str, raw_data: dict[str, Any] | None = None
-    ) -> None:
-        """Initialize a config object."""
-        self.auth = auth
+    def __init__(self, site_id: str, raw_data: dict[str, Any]) -> None:
+        """Initialize a status object."""
         self.site_id = site_id
-        self.raw_data = {} if raw_data is None else raw_data
+        self.raw_data = raw_data
+
+    def __eq__(self, other: "EnergySiteStatus") -> bool:
+        """Compare EnergySitestatus objects."""
+        return (
+            self.site_id == other.site_id
+            and self.percentage_charged == other.percentage_charged
+            and self.solar_power == other.solar_power
+            and self.battery_power == other.battery_power
+            and self.load_power == other.load_power
+            and self.grid_power == other.grid_power
+            and self.generator_power == other.generator_power
+            and self.grid_status == other.grid_status
+            and self.island_status == other.island_status
+            and self.storm_mode_active == other.storm_mode_active
+            # Strictly we should check the timestamp too
+            # self.timestamp == other.timestamp
+        )
 
     @property
     def percentage_charged(self) -> float:
@@ -181,36 +204,24 @@ class EnergySiteStatus:
             wcdict[j["din"]] = WallConnector(j)
         return wcdict
 
-    async def async_update(self) -> None:
-        """Update the energy site status.
-
-        Note that this retreives the full configuration.
-        """
-        resp = await self.auth.request("GET", f"{self.site_id}/config")
-        resp.raise_for_status()
-        self.raw_data = (await resp.json())["live_status"]
-
 
 class EnergySiteConfig:
     """Class that represents an energy site's configuration."""
 
-    def __init__(
-        self, auth: Auth, site_id: str, raw_data: dict[str, Any] | None = None
-    ) -> None:
-        """Initialize a config object.
-
-        If initialized with default raw_data, then the property calls
-        will fail. The only reason this is supported is to allow an
-        EnergySiteConfig to be instantiated, and then call
-        async_update or async_control on it. After this, property
-        usage will behave normally. This also allows the site
-        configuration to be set without having to get the current
-        configuration first.
-
-        """
-        self.auth = auth
+    def __init__(self, site_id: str, raw_data: dict[str, Any]) -> None:
+        """Initialize a config object."""
         self.site_id = site_id
-        self.raw_data = {} if raw_data is None else raw_data
+        self.raw_data = raw_data
+
+    def __eq__(self, other: "EnergySiteConfig") -> bool:
+        """Compare EnergySiteConfig objects."""
+        return (
+            self.site_id == other.site_id
+            and self.backup_reserve_percent == other.backup_reserve_percent
+            and self.operational_mode == other.operational_mode
+            and self.energy_exports == other.energy_exports
+            and self.grid_charging == other.grid_charging
+        )
 
     @property
     def backup_reserve_percent(self) -> int:
@@ -241,9 +252,24 @@ class EnergySiteConfig:
     @property
     def live_status(self) -> EnergySiteStatus:
         """A class from which live status of the site can be queried."""
-        return EnergySiteStatus(self.auth, self.site_id, self.raw_data["live_status"])
+        return EnergySiteStatus(self.site_id, self.raw_data["live_status"])
 
-    async def async_control(self, **kwargs) -> None:
+
+class EnergySite:
+    """Class representing a single energy site."""
+
+    def __init__(self, auth: Auth, site_id: str) -> None:
+        """Initialize the API and store the auth so we can make requests."""
+        self.auth = auth
+        self.site_id = site_id
+
+    async def async_get_config(self) -> EnergySiteConfig:
+        """Return the energy site configuration."""
+        resp = await self.auth.request("GET", f"{self.site_id}/config")
+        resp.raise_for_status()
+        return EnergySiteConfig(self.site_id, await resp.json())
+
+    async def async_set_config(self, **kwargs) -> EnergySiteConfig:
         """Reconfigure the energy site with new parameters.
 
         Each parameter can be changed individually, or together.
@@ -270,25 +296,5 @@ class EnergySiteConfig:
             json["operational_mode"] = str(value)
         resp = await self.auth.request("POST", f"{self.site_id}/config", json=json)
         resp.raise_for_status()
-        self.raw_data = await resp.json()
 
-    async def async_update(self) -> None:
-        """Update the energy site configuration."""
-        resp = await self.auth.request("GET", f"{self.site_id}/config")
-        resp.raise_for_status()
-        self.raw_data = await resp.json()
-
-
-class EnergySite:
-    """Class representing a single energy site."""
-
-    def __init__(self, auth: Auth, site_id: str) -> None:
-        """Initialize the API and store the auth so we can make requests."""
-        self.auth = auth
-        self.site_id = site_id
-
-    async def async_get_config(self) -> EnergySiteConfig:
-        """Return the energy site configuration."""
-        resp = await self.auth.request("GET", f"{self.site_id}/config")
-        resp.raise_for_status()
-        return EnergySiteConfig(self.auth, self.site_id, await resp.json())
+        return EnergySiteConfig(self.site_id, await resp.json())

custom_components/powerwall_control/number.py
@@ -44,7 +44,7 @@ class PwCtrlBackupReserveNumberEntity(CoordinatorEntity, NumberEntity):
     @callback
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
-        self._attr_native_value = self.coordinator.config.backup_reserve_percent
+        self._attr_native_value = self.coordinator.data.backup_reserve_percent
         self._attr_icon = icon_for_battery_level(self._attr_native_value)
         self.async_write_ha_state()
 

custom_components/powerwall_control/select.py
@@ -40,7 +40,7 @@ class PwCtrlOperationalModeSelectEntity(CoordinatorEntity, SelectEntity):
     @callback
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
-        mode = self.coordinator.config.operational_mode
+        mode = self.coordinator.data.operational_mode
         if mode == OperationalMode.AUTONOMOUS:
             self._attr_current_option = "auto"
         elif mode == OperationalMode.BACKUP:
@@ -87,7 +87,7 @@ class PwCtrlExportModeSelectEntity(CoordinatorEntity, SelectEntity):
     @callback
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
-        mode = self.coordinator.config.energy_exports
+        mode = self.coordinator.data.energy_exports
         if mode == EnergyExportMode.BATTERY_OK:
             self._attr_current_option = "battery_ok"
         elif mode == EnergyExportMode.PV_ONLY:

custom_components/powerwall_control/switch.py
@@ -43,7 +43,7 @@ class PwCtrlGridChargingSwitch(CoordinatorEntity, SwitchEntity):
     @callback
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
-        self._is_on = self.coordinator.config.grid_charging
+        self._is_on = self.coordinator.data.grid_charging
         self.async_write_ha_state()
 
     async def async_turn_on(self, **kwargs) -> None:

powerwall.py
@@ -131,11 +131,12 @@ async def main():
 
     async with aiohttp.ClientSession() as session:
         auth = netzero.Auth(session, args.api_token)
-        config = netzero.EnergySiteConfig(auth, args.system_id)
+        site = netzero.EnergySite(auth, args.system_id)
 
+        config = None
         if request:
             print(f"Changing Powerwall state...\n{request}")
-            await config.async_control(
+            config = await site.async_set_config(
                 backup_reserve_percent=args.set_backup,
                 grid_charging=args.grid_charging,
                 energy_exports=export_mode,
@@ -144,7 +145,8 @@ async def main():
 
         else:
             print("Reading Powerwall state...")
-            await config.async_update()
+            config = await site.async_get_config()
+        assert config
 
         print(f"""Configuration:
   Battery backup reserve: {config.backup_reserve_percent}%

tests/test_netzero.py
@@ -153,7 +153,7 @@ def test_energy_site_config_values():
     charging = [True, False, True]
 
     for i, c in enumerate(json_cfg):
-        config = netzero.EnergySiteConfig(None, None, c)
+        config = netzero.EnergySiteConfig(12345, c)
 
         assert config.backup_reserve_percent == backup[i]
         assert config.operational_mode == mode[i]
@@ -161,44 +161,8 @@ def test_energy_site_config_values():
         assert config.grid_charging == charging[i]
 
 
-async def test_energy_site_config_update():
-    """Test EnergySiteConfig async_update."""
-    token = "abcdef"
-    system_id = 12345
-    expected_response = {
-        "backup_reserve_percent": 80,
-        "operational_mode": "autonomous",
-        "energy_exports": "pv_only",
-        "grid_charging": True,
-    }
-
-    with aioresponses() as mock:
-        mock.get(
-            f"https://api.netzero.energy/api/v1/{system_id}/config",
-            status=200,
-            payload=expected_response,
-        )
-        async with aiohttp.ClientSession() as session:
-            auth = netzero.Auth(session, token)
-            config = netzero.EnergySiteConfig(auth, system_id)
-            await config.async_update()
-
-            assert config.backup_reserve_percent == 80
-            assert config.operational_mode == netzero.OperationalMode.AUTONOMOUS
-            assert config.energy_exports == netzero.EnergyExportMode.PV_ONLY
-            assert config.grid_charging
-
-            mock.assert_called_once_with(
-                f"https://api.netzero.energy/api/v1/{system_id}/config",
-                headers={
-                    "authorization": f"Bearer {token}",
-                },
-                allow_redirects=True,
-            )
-
-
-async def test_energy_site_config_control():
-    """Test EnergySiteConfig async_control."""
+async def test_energy_site_set_config():
+    """Test EnergySite async_set_config."""
     token = "abcdef"
     system_id = 12345
     expected_response = {
@@ -217,9 +181,9 @@ async def test_energy_site_config_control():
         )
         async with aiohttp.ClientSession() as session:
             auth = netzero.Auth(session, token)
-            config = netzero.EnergySiteConfig(auth, system_id)
+            site = netzero.EnergySite(auth, system_id)
 
-            await config.async_control(backup_reserve_percent=100)
+            _ = await site.async_set_config(backup_reserve_percent=100)
             mock.assert_called_once_with(
                 f"https://api.netzero.energy/api/v1/{system_id}/config",
                 method="POST",
@@ -230,7 +194,7 @@ async def test_energy_site_config_control():
                 allow_redirects=True,
             )
 
-            await config.async_control(
+            _ = await site.async_set_config(
                 operational_mode=netzero.OperationalMode.SELF_CONSUMPTION
             )
             mock.assert_called_with(
@@ -243,7 +207,9 @@ async def test_energy_site_config_control():
                 allow_redirects=True,
             )
 
-            await config.async_control(energy_exports=netzero.EnergyExportMode.NEVER)
+            _ = await site.async_set_config(
+                energy_exports=netzero.EnergyExportMode.NEVER
+            )
             mock.assert_called_with(
                 f"https://api.netzero.energy/api/v1/{system_id}/config",
                 method="POST",
@@ -254,7 +220,7 @@ async def test_energy_site_config_control():
                 allow_redirects=True,
             )
 
-            await config.async_control(grid_charging=False)
+            _ = await site.async_set_config(grid_charging=False)
             mock.assert_called_with(
                 f"https://api.netzero.energy/api/v1/{system_id}/config",
                 method="POST",
@@ -297,8 +263,8 @@ async def test_energy_site_live_status():
         )
         async with aiohttp.ClientSession() as session:
             auth = netzero.Auth(session, token)
-            config = netzero.EnergySiteConfig(auth, system_id)
-            await config.async_update()
+            site = netzero.EnergySite(auth, system_id)
+            config = await site.async_get_config()
 
             status = config.live_status
             assert status.percentage_charged == 100.0
@@ -386,7 +352,7 @@ def test_energy_site_status_values():
         datetime.datetime(2022, 2, 28, 12, 0, 0, 0, tzinfo=datetime.UTC),
     ]
     for i, s in enumerate(json_status):
-        status = netzero.EnergySiteStatus(None, None, s)
+        status = netzero.EnergySiteStatus(12345, s)
 
         assert status.percentage_charged == charged[i]
         assert status.solar_power == solar[i]
@@ -398,62 +364,6 @@ def test_energy_site_status_values():
         assert status.island_status == island[i]
         assert status.storm_mode_active == storm[i]
         assert status.timestamp == time[i]
-
-
-async def test_energy_site_status_update():
-    """Test EnergySiteStatus async_update."""
-    token = "abcdef"
-    system_id = 12345
-    expected_response = {
-        "backup_reserve_percent": 80,
-        "operational_mode": "autonomous",
-        "energy_exports": "pv_only",
-        "grid_charging": True,
-        "live_status": {
-            "percentage_charged": 100.0,
-            "solar_power": 4140,
-            "battery_power": -2520,
-            "load_power": 1620,
-            "grid_power": 110,
-            "generator_power": 40,
-            "grid_status": "Active",
-            "island_status": "on_grid",
-            "storm_mode_active": False,
-            "timestamp": "2020-12-31T23:59:59.900Z",
-        },
-    }
-
-    with aioresponses() as mock:
-        mock.get(
-            f"https://api.netzero.energy/api/v1/{system_id}/config",
-            status=200,
-            payload=expected_response,
-        )
-        async with aiohttp.ClientSession() as session:
-            auth = netzero.Auth(session, token)
-            status = netzero.EnergySiteStatus(auth, system_id)
-            await status.async_update()
-
-            assert status.percentage_charged == 100.0
-            assert status.solar_power == 4140
-            assert status.battery_power == -2520
-            assert status.load_power == 1620
-            assert status.grid_power == 110
-            assert status.generator_power == 40
-            assert status.grid_status == netzero.GridStatus.ACTIVE
-            assert status.island_status == netzero.IslandStatus.ON_GRID
-            assert not status.storm_mode_active
-            assert status.timestamp == datetime.datetime(
-                2020, 12, 31, 23, 59, 59, 900000, tzinfo=datetime.UTC
-            )
-
-            mock.assert_called_once_with(
-                f"https://api.netzero.energy/api/v1/{system_id}/config",
-                headers={
-                    "authorization": f"Bearer {token}",
-                },
-                allow_redirects=True,
-            )
 
 
 async def test_energy_site_status_wall_connectors():
@@ -501,8 +411,9 @@ async def test_energy_site_status_wall_connectors():
         )
         async with aiohttp.ClientSession() as session:
             auth = netzero.Auth(session, token)
-            status = netzero.EnergySiteStatus(auth, system_id)
-            await status.async_update()
+            site = netzero.EnergySite(auth, system_id)
+            config = await site.async_get_config()
+            status = config.live_status
 
             wc = status.wall_connectors
             assert "abcd" in wc

commit 587523cfebf423d6cb8f38adfb4ac9a1607fe5e8

coordinator: add unit test
Try and test the async_request_control() behaviour of PwCtrlCoordinator.
Update the mock to add a separate response for async_set_config().

Fix a bug introduced by commit d5a66cca80 - the use of `config` should
have been updated to use `site`

custom_components/powerwall_control/coordinator.py
@@ -77,7 +77,7 @@ class PwCtrlCoordinator(DataUpdateCoordinator[netzero.EnergySiteConfig]):
             return
 
         # Pass the accumulated configuration changes to netzero
-        updated_config = await self.config.async_set_config(**self._reconfig_dict)
+        updated_config = await self.site.async_set_config(**self._reconfig_dict)
         self._reconfig_dict.clear()
 
         # Update listeners with any new values

tests/conftest.py
@@ -22,9 +22,15 @@ def auto_enable_custom_integrations(enable_custom_integrations):
     return
 
 
-DEFAULT_JSON = {
+DEFAULT_GET_CONFIG = {
     "backup_reserve_percent": 80,
     "operational_mode": "autonomous",
+    "energy_exports": "pv_only",
+    "grid_charging": False,
+}
+DEFAULT_SET_CONFIG = {
+    "backup_reserve_percent": 70,
+    "operational_mode": "self_consumption",
     "energy_exports": "pv_only",
     "grid_charging": False,
 }
@@ -34,7 +40,8 @@ DEFAULT_JSON = {
 async def mock_energysite_fixture(hass: HomeAssistant) -> AsyncMock:
     """This fixture provides a mock EnergySite, returning constant EnergySiteConfig."""
     mock_energysite = await _mock_energysite_get_config(
-        EnergySiteConfig(123456, DEFAULT_JSON)
+        EnergySiteConfig(123456, DEFAULT_GET_CONFIG),
+        EnergySiteConfig(123456, DEFAULT_SET_CONFIG),
     )
 
     entry = MockConfigEntry(

tests/mocks.py
@@ -5,11 +5,12 @@ from unittest.mock import AsyncMock
 from custom_components.powerwall_control.netzero import EnergySite
 
 
-async def _mock_energysite_get_config(config):
+async def _mock_energysite_get_config(get_config, set_config):
     """Mock netzero EnergySite."""
     energysite_mock = AsyncMock(EnergySite)
     # energysite_mock.__aenter__.return_value = energysite_mock
 
-    energysite_mock.async_get_config.return_value = config
+    energysite_mock.async_get_config.return_value = get_config
+    energysite_mock.async_set_config.return_value = set_config
 
     return energysite_mock

tests/test_coordinator.py
@@ -1,0 +1,112 @@
+"""Tests for the control coordinator.
+
+This does not re-test the functionality inheritted from the
+DataUpdatecoordinator.
+"""
+
+from unittest.mock import patch
+
+import pytest
+from pytest_homeassistant_custom_component.common import async_fire_time_changed
+
+from custom_components.powerwall_control import netzero
+from custom_components.powerwall_control.coordinator import PwCtrlCoordinator
+from homeassistant.core import HomeAssistant
+from homeassistant.util.dt import utcnow
+
+
+def get_crd(
+    hass: HomeAssistant,
+) -> PwCtrlCoordinator:
+    """Make coordinator mocks."""
+    site = netzero.EnergySite(None, 123456)
+
+    return PwCtrlCoordinator(hass, site)
+
+
+@pytest.fixture
+def crd(hass: HomeAssistant, mock_energysite: netzero.EnergySite) -> PwCtrlCoordinator:
+    """Coordinator mock with default update interval."""
+    return get_crd(hass)
+
+
+async def test_async_request_control(
+    hass: HomeAssistant,
+    crd: PwCtrlCoordinator,
+) -> None:
+    """Test async_request_control for coordinator."""
+    assert crd.data is None
+    await crd.async_refresh()
+    assert crd.last_update_success is True
+
+    update_interval = crd.update_interval
+
+    # Check initial values
+    assert crd.data.backup_reserve_percent == 80
+    assert crd.data.operational_mode == netzero.OperationalMode.AUTONOMOUS
+    assert crd.data.energy_exports == netzero.EnergyExportMode.PV_ONLY
+    assert not crd.data.grid_charging
+
+    updates = []
+
+    def update_callback():
+        updates.append(crd.data)
+
+    unsub = crd.async_add_listener(update_callback)
+    await crd.async_request_control(backup_reserve_percent=50)
+
+    # Nothing should have changed yet
+    assert len(updates) == 0
+
+    # Wait for the update interval
+    async_fire_time_changed(hass, utcnow() + update_interval)
+    await hass.async_block_till_done()
+
+    # Check we've had an update from the set_config call
+    assert len(updates) == 1
+    assert updates[0].backup_reserve_percent == 70
+    assert updates[0].operational_mode == netzero.OperationalMode.SELF_CONSUMPTION
+    assert updates[0].energy_exports == netzero.EnergyExportMode.PV_ONLY
+    assert not updates[0].grid_charging
+
+    # Unsubscribe
+    unsub()
+
+
+async def test_shutdown(hass: HomeAssistant, crd: PwCtrlCoordinator) -> None:
+    """Test async_shutdown for coordinator."""
+    assert crd.data is None
+    await crd.async_refresh()
+    assert crd.last_update_success is True
+
+    # Check initial values
+    assert crd.data.backup_reserve_percent == 80
+    assert crd.data.operational_mode == netzero.OperationalMode.AUTONOMOUS
+    assert crd.data.energy_exports == netzero.EnergyExportMode.PV_ONLY
+    assert not crd.data.grid_charging
+
+    updates = []
+
+    def update_callback():
+        updates.append(crd.data)
+
+    _ = crd.async_add_listener(update_callback)
+    await crd.async_request_control()
+
+    # Nothing updated yet
+    assert len(updates) == 0
+
+    # Test shutdown through function
+    # ruff: noqa: SLF001 allow use of private _debounced_control
+    with patch.object(crd._debounced_control, "async_shutdown") as mock_shutdown:
+        await crd.async_shutdown()
+
+    # Test we shutdown the debouncer and cleared the subscriptions
+    assert len(mock_shutdown.mock_calls) == 1
+
+    # Wait for the update interval
+    async_fire_time_changed(hass, utcnow() + crd.update_interval)
+    await hass.async_block_till_done()
+
+    # No updates
+    assert len(updates) == 0