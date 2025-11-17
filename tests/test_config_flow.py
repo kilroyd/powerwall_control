721e0920a805012e35f3639a3ad960ae75f3db64
David Kilroy <kilroyd@googlemail.com>
2025-11-17 22:18:20 +0000

    Enable reconfiguration

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -3,6 +3,7 @@
 from unittest.mock import patch
 
 import aiohttp
+from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from custom_components.powerwall_control.const import DOMAIN
 from homeassistant import config_entries
@@ -113,8 +114,8 @@ async def test_flow_user_step_cannot_connect(hass: HomeAssistant) -> None:
     assert result["step_id"] == "user"
 
     # Patch async_get_config - this verifies that we can talk to the
-    # servers.  Just return without error, which should allow the
-    # config flow to get to the next step.
+    # servers. Return an error, preventing the config flow getting to
+    # the next step.
     with patch(
         "custom_components.powerwall_control.config_flow.async_get_config",
         side_effect=aiohttp.ClientResponseError(
@@ -127,6 +128,139 @@ async def test_flow_user_step_cannot_connect(hass: HomeAssistant) -> None:
                 "api_token": "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCD",
                 "system_id": "1234567",
             },
+        )
+        await hass.async_block_till_done()
+
+    assert result["errors"] == {"base": "cannot_connect"}
+
+
+async def test_flow_reconfigure_step_valid_input(hass: HomeAssistant) -> None:
+    """Test reconfigure with valid input proceeds to async_update_reload_and_abort."""
+
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "api_token": "ABCDEFG",
+            "system_id": "1234567",
+        },
+    )
+    entry.add_to_hass(hass)
+    result = await entry.start_reconfigure_flow(hass)
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "reconfigure"
+
+    # Patch async_get_config - this verifies that we can talk to the
+    # servers.  Just return without error, which should allow the
+    # config flow to get to the next step.
+    with patch(
+        "custom_components.powerwall_control.config_flow.async_get_config",
+        return_value=(None, None),
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], user_input=VALID_INPUT
+        )
+        await hass.async_block_till_done()
+
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "reconfigure_successful"
+    assert entry.data["api_token"] == VALID_INPUT["api_token"]
+    assert entry.data["system_id"] == VALID_INPUT["system_id"]
+
+
+async def test_flow_reconfigure_step_bad_api_token(hass: HomeAssistant) -> None:
+    """Test reconfigure with bad API token."""
+
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "api_token": "ABCDEFG",
+            "system_id": "1234567",
+        },
+    )
+    entry.add_to_hass(hass)
+    result = await entry.start_reconfigure_flow(hass)
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "reconfigure"
+
+    # Patch async_get_config - this verifies that we can talk to the
+    # servers.  Just return without error, which should allow the
+    # config flow to get to the next step.
+    with patch(
+        "custom_components.powerwall_control.config_flow.async_get_config",
+        return_value=(None, None),
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], user_input={"api_token": "short", "system_id": "1234567"}
+        )
+        await hass.async_block_till_done()
+
+    assert result["errors"] == {"base": "invalid_token"}
+
+
+async def test_flow_reconfigure_step_bad_system_id(hass: HomeAssistant) -> None:
+    """Test reconfigure with bad system id."""
+
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "api_token": "ABCDEFG",
+            "system_id": "1234567",
+        },
+    )
+    entry.add_to_hass(hass)
+    result = await entry.start_reconfigure_flow(hass)
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "reconfigure"
+
+    # Patch async_get_config - this verifies that we can talk to the
+    # servers.  Just return without error, which should allow the
+    # config flow to get to the next step.
+    with patch(
+        "custom_components.powerwall_control.config_flow.async_get_config",
+        return_value=(None, None),
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input={
+                "api_token": "abcdefghijklmnopqrstuvwxyz0123456789abcd",
+                "system_id": "alpha",
+            },
+        )
+        await hass.async_block_till_done()
+
+    assert result["errors"] == {"base": "invalid_system_id"}
+
+
+async def test_flow_reconfigure_step_cannot_connect(hass: HomeAssistant) -> None:
+    """Test reconfigure with invalid token and system ID."""
+
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "api_token": "ABCDEFG",
+            "system_id": "1234567",
+        },
+    )
+    entry.add_to_hass(hass)
+    result = await entry.start_reconfigure_flow(hass)
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "reconfigure"
+
+    # Patch async_get_config - this verifies that we can talk to the
+    # servers. Return an error, preventing the config flow getting to
+    # the next step.
+    with patch(
+        "custom_components.powerwall_control.config_flow.async_get_config",
+        side_effect=aiohttp.ClientResponseError(
+            "Can't connect", status=400, history=()
+        ),
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], user_input=VALID_INPUT
         )
         await hass.async_block_till_done()
 
